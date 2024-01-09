6d65da4366491ef11351ec6b4a5db1f812341331
LAMPA23 <bimeebim@gmail.com>
2024-01-09 22:25:34 +0200

    -

--- 4/python/scripts/apply_centroids.py
+++ 4/python/scripts/apply_centroids.py
@@ -23,8 +23,7 @@ def apply_centroids(path_to_xlsx, workbook, sheet_name, centroids, noise_matrix,
         for cnt in range(3):
             if Euclidean_distance[row, cnt] == min_evclid_distance:
                 ans[row,cnt] = 1
-    
-
+                
 
     # Error check
     for row in range(225):
@@ -39,23 +38,37 @@ def apply_centroids(path_to_xlsx, workbook, sheet_name, centroids, noise_matrix,
     # Create new sheet
     sheet = workbook.create_sheet(title=sheet_name) 
 
-    # Centring a text in cell
-    for row in range(676):
-        for column in range(8):
-            sheet.cell(row=row+1, column=column+1).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+    # Centring a text and add value in cell)
+    for row in range(675):
+        for column in range(6):
+            if column < 3:
+                sheet.cell(row=row+1, column=column+1, value=ans[row,column]).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+            else:
+                sheet.cell(row=row+1, column=column+1, value=corect_ans[row,column - 3]).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
 
-    # Add table
-    sheet.cell(row=1, column=1, value='Символ №1').fill = openpyxl.styles.PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
-    sheet.cell(row=1, column=2, value='Символ №2').fill = openpyxl.styles.PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
-    sheet.cell(row=1, column=3, value='Символ №3').fill = openpyxl.styles.PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
+            if column < 3:
+                sheet.cell(row=row+1, column=column+1).fill = openpyxl.styles.PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+            else:
+                sheet.cell(row=row+1, column=column+1).fill = openpyxl.styles.PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
+
+        if not np.all(corect_ans[row,:] == ans[row,:]):
+            for cnt in range(6):
+                sheet.cell(row=row+1, column=cnt+1).fill = openpyxl.styles.PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")           
 
 
-    # # Add color and value to matrix
-    # for row in range(675):
-    #     sheet.cell(row=row+2, column=1, value=target_data[row,0]).fill = openpyxl.styles.PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
-    #     sheet.cell(row=row+2, column=2, value=target_data[row,1]).fill = openpyxl.styles.PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
-    #     sheet.cell(row=row+2, column=3, value=target_data[row,2]).fill = openpyxl.styles.PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
-
+    # Add table tilte and color to table tilte
+    sheet.cell(row=1, column=1, value='Правильне значення №1').fill = openpyxl.styles.PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+    sheet.cell(row=1, column=2, value='Правильне значення №2').fill = openpyxl.styles.PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+    sheet.cell(row=1, column=3, value='Правильне значення №3').fill = openpyxl.styles.PatternFill(start_color="00FF00", end_color="00FF00", fill_type="solid")
+    sheet.cell(row=1, column=4, value='Отримане значення №1').fill = openpyxl.styles.PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
+    sheet.cell(row=1, column=5, value='Отримане значення №2').fill = openpyxl.styles.PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
+    sheet.cell(row=1, column=6, value='Отримане значення №3').fill = openpyxl.styles.PatternFill(start_color="0000FF", end_color="0000FF", fill_type="solid")
+    
+    for cnt in range(3):
+        sheet.cell(row=1, column=cnt+8, value=f'К-сть помилок №{cnt+1}').fill = openpyxl.styles.PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+        sheet.cell(row=2, column=cnt+8, value=err[cnt]).fill = openpyxl.styles.PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+        sheet.cell(row=1, column=cnt+8).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+        sheet.cell(row=2, column=cnt+8).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
 
     # Save data to Excel   
     workbook.save(path_to_xlsx)
--- 4/python/scripts/centroids_separated_check.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-import openpyxl
-
-def centroids_separated_check(path_to_xlsx, workbook, noise_matrix_all, centroids):
-    pass
