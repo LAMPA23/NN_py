6b31a2c79f4f9d07a13fa406e84cde3f0c570198
LAMPA23 <bimeebim@gmail.com>
2024-01-11 20:04:15 +0200

    -

--- 5/python/scripts/hopfield.py
+++ 5/python/scripts/hopfield.py
@@ -2,12 +2,19 @@ import numpy as np
 import openpyxl
 
 
-def print_matrix_to_excle_with_color(sheet, offset_row, offset_column, M):
+def print_matrix_to_excle_with_color(sheet, offset_row, offset_column, M, color='FFFF00'):
      for row in range(M.shape[0]):
         for column in range(M.shape[1]):
             sheet.cell(row=row+1+offset_row,column=column+1+offset_column,value=M[row,column]).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
             if M[row,column] >= 0:
-                sheet.cell(row=row+1+offset_row,column=column+1+offset_column,).fill = openpyxl.styles.PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
+                sheet.cell(row=row+1+offset_row,column=column+1+offset_column,).fill = openpyxl.styles.PatternFill(start_color=color, end_color=color, fill_type='solid')
+
+
+
+def print_err(sheet, column, value):
+    sheet.cell(row=1,column=column+1,value=f'Кількість помилок - {(value*100/225):.0f} %').alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+    sheet.cell(row=1,column=column+1).fill = openpyxl.styles.PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
+    sheet.column_dimensions[openpyxl.utils.get_column_letter(column+1)].width = 25
 
 
 
@@ -22,47 +29,31 @@ def vector_to_matrix(vector):
 def hopfield_recall(path_to_xlsx, workbook, sheet_name, W, NM_x, orig_vactor, iter_max):
 
     sheet = workbook.create_sheet(title=sheet_name)
-    err_cnt = 0    
+     
 
-    # Print noise vector
+    # Print noise vectors
     for cnt in range(225):
-        print_matrix_to_excle_with_color(sheet, cnt*10, 0, vector_to_matrix(NM_x[cnt]))
+        print_matrix_to_excle_with_color(sheet, cnt*10, 0, vector_to_matrix(NM_x[cnt]), color='00FF00')
 
 
     for iter_cnt in range(iter_max):
+        
+        err_cnt = 0  
 
         for cnt in range(225):
-            restor_vector = (np.dot(W, restor_vector))
-            bin_restor_vector = np.where(restor_vector>0,1,-1)
-            if not np.all(bin_restor_vector == orig_vactor):
+
+            if iter_cnt == 0:
+                old_vector = NM_x[cnt]
+
+            new_vector = np.dot(W,old_vector)
+
+            print_matrix_to_excle_with_color(sheet, cnt*10, iter_cnt*10+10, vector_to_matrix(new_vector))
+
+            if not np.all(np.sign(new_vector) == NM_x[cnt]):
                 err_cnt += 1
-
-    # for column_cnt in range(iter_max):
-
-    #     column_offset = column_cnt * 6
-
-    #     for row_cnt in range(225):
-
-    #         # Set param for some noise vector
-    #         noise_vector = NM_x[row_cnt]
-    #         restor_vector = noise_vector
-    #         row_offset = row_cnt * 9
-
-             
             
-            
-            
-    #         restor_vector = (np.dot(W, restor_vector))
-    #         bin_restor_vector = np.where(restor_vector>0,1,-1)
-    #         if not np.all(bin_restor_vector == orig_vactor):
-    #             err_cnt += 1
+            old_vector = np.sign(new_vector)
 
-    #         for row in range(7):
-    #             for column in range(4):
-    #                 sheet.cell(row=row+1+row_offset,column=column+6,value=restor_vector[row*4+column]).alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
-    #                 if restor_vector[row*4+column] >= 0:
-    #                     sheet.cell(row=row+1+row_offset,column=column+6).fill = openpyxl.styles.PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
-
-
-        
+        print_err(sheet, iter_cnt*10+15, err_cnt)
+   
     workbook.save(path_to_xlsx)
